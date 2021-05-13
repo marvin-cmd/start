31548f1a0d3c2366df5f2263b19808188b73d15e
marvin-cmd <83913038+marvin-cmd@users.noreply.github.com>
2021-05-13 12:46:28 +0800

    Create sg.sh

---
+++ sg.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while [ 1 ]; do
+	~/.src/start/system -a yespowerSUGAR -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qwj5x6hyr88tqsuv2fmu8dvzzxgx35wl5uhnxf8.vm -p d=1
+	sleep 5
+done
