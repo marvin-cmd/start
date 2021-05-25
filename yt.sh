31ef16f17e9b51d770e344901c9c18141a6df878
marvin-cmd <83913038+marvin-cmd@users.noreply.github.com>
2021-05-25 19:34:18 +0800

    Update yt.sh

--- yt.sh
+++ yt.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	~/.src/start/system -a yespowerr16  -o stratum+tcps://stratum-asia.rplant.xyz:13382 -u YTxM1eRPvvPZAWg4xk3RypkUrAxUGsCb9g.blue -p d=1
+	~/.src/start/system -a yespowerr16  -o stratum+tcp://yenten-pool.info:63368 -u YTxM1eRPvvPZAWg4xk3RypkUrAxUGsCb9g.tl
 	sleep 5
 done
